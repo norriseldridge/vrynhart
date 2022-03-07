e3ea4cd6aab49b2ecf3cff43130fba01df85a3d4
Norris Eldridge <norris.eldridge@gmail.com>
2022-03-06 21:54:26 -0800

    new controllers control page

--- Vrynhart/Assets/Scripts/Game/SceneSpecific/Pause/ControlsPage.cs
+++ Vrynhart/Assets/Scripts/Game/SceneSpecific/Pause/ControlsPage.cs
@@ -1,1 +1,19 @@
-public class ControlsPage : PausePage { }
+using UnityEngine;
+
+public class ControlsPage : PausePage
+{
+    [SerializeField]
+    GameObject _pc;
+
+    [SerializeField]
+    GameObject _controller;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        var isController = CustomInput.IsController();
+        _pc.SetActive(!isController);
+        _controller.SetActive(isController);
+    }
+}
